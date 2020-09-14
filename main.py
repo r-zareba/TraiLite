07d1220fd59bc3122e96906979cb88b303628652
r-zareba <r.zareba@outlook.com>
2020-09-14 10:07:40 +0200

    Delete celery and geckodriver log files after restart

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ import subprocess
 
 if __name__ == '__main__':
     os.chdir('./app_scheduler')
+    os.remove('celerybeat-schedule')
+    os.remove('geckodriver.log')
     subprocess.run('celery -A tasks worker --beat --loglevel=WARNING',
                    shell=True)
